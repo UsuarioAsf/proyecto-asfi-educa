c52ea7ca19caa536822665cb109d0c39fd399115
UsuarioAsf <usuarioasfi@gmail.com>
2023-01-03 17:13:33 -0400

    version final

--- src/ahorcado/js/common.js
+++
@@ -1,7 +0,0 @@
-
-// Tomado de https://parzibyte.me/blog/2019/06/26/menu-responsivo-bootstrap-4-sin-dependencias/
-
-const LOCAL_STORAGE_WORDS_KEY = "words";
-let bancoRespuestas = ["inversion", "banco", "bursatil", "asfi", "aj","Amortizacion","Cheque"];
-const getWords = () => (bancoRespuestas);
-const saveWords = (words) => (localStorage.setItem(LOCAL_STORAGE_WORDS_KEY, JSON.stringify(words)));
